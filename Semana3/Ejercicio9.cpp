ce772ef33325c3f5c242dbaeed842aa5247ac21d
Nícolas Castillo <ncastilloraggio@gmail.com>
2020-07-29 21:24:44 -0500

    Añadido el comentario de getch() para Windows

--- Semana3/Ejercicio9.cpp
+++ Semana3/Ejercicio9.cpp
@@ -43,4 +43,5 @@ int main(int argc, char* argv[])
 	cout << cantidadEfectivo[6] << " billetes de " << valorEfectivo[6] << " soles." << endl;
 
     return 0;
+//  getch();    (Descomentar en Windows)
 }
